be34f6c63f175298874d7b75b11fa4274595048f
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-28 10:05:22 +0100

    Commit 3 dynamic templates

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title':'',
+        'location':'',
+        'salary':'',
+    },
+    {
+        'id': 2,
+        'title':'',
+        'location':'',
+        'salary':'',
+    },
+    {
+        'id': 3,
+        'title':'',
+        'location':'',
+        'salary':'',
+    },
+]
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS, company_name='')
 
+@app.route('/api/jobs')
+def hello_world():  # put application's code here
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
