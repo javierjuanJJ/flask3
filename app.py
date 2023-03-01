03835a6f39fdbe1eeb75e90cce3ecf5a582e857b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-01 11:37:29 +0100

    Commit 5 db connection

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 from flask import Flask, render_template, jsonify
 
+import database
+
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': 'Rs. 10,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Delhi, India',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote'
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco, USA',
-        'salary': '$150,000'
-    }
-]
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': 'Data Analyst',
+#         'location': 'Bengaluru, India',
+#         'salary': 'Rs. 10,00,000'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Data Scientist',
+#         'location': 'Delhi, India',
+#         'salary': 'Rs. 15,00,000'
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Frontend Engineer',
+#         'location': 'Remote'
+#     },
+#     {
+#         'id': 4,
+#         'title': 'Backend Engineer',
+#         'location': 'San Francisco, USA',
+#         'salary': '$150,000'
+#     }
+# ]
+
+JOBS = database.load_jobs_from_db()
 
 @app.route('/')
 def hello_world():  # put application's code here
