0fbccc717e92402c88f5ed2cd97e6198668bbab3
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-02 09:47:37 +0100

    Commit 7 show_job

--- app.py
+++ app.py
@@ -41,5 +41,19 @@ def hello_world():  # put application's code here
 def list_jobs():  # put application's code here
     return jsonify(JOBS)
 
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = database.load_job_from_db(id)
+
+    if not job:
+        return "Not Found", 404
+
+    return render_template('jobItem.html',
+                           job=job)
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
